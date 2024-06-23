34384ad3d6be8ee152d56b141c0652929be0f39c
4kpros <prosper.abouar@gmail.com>
2024-06-23 10:44:29 +0100

    config(env): add env variable and set port

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
-	"fmt"
-
+	"github.com/4kpros/go-crud/initializers"
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	initializers.LoadEnvVars()
+}
+
 func main() {
 	r := gin.Default()
-	r.GET("/ping", apiTest)
+	r.GET("/", apiTest)
 	r.Run()
-	fmt.Println("API Server running ...")
 }
 
 func apiTest(c *gin.Context) {
 	c.JSON(200, gin.H{
-		"message": "pong",
+		"message": "Welcome to GO-CRUD API",
 	})
 }
